ebe76ad81ecd38fe2e42ba77ef606d54187f00c8
Dimityr <dzambazov1986@gmail.com>
2024-03-23 18:54:16 +0200

    Add project files.

---
+++ Articles/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Article
+{
+    public string Title { get; private set; }
+    public string Content { get; private set; }
+    public string Author { get; private set; }
+
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] articleInfo = Console.ReadLine().Split(", ");
+        Article article = new Article(articleInfo[0], articleInfo[1], articleInfo[2]);
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] command = Console.ReadLine().Split(": ");
+            switch (command[0])
+            {
+                case "Edit":
+                    article.Edit(command[1]);
+                    break;
+                case "ChangeAuthor":
+                    article.ChangeAuthor(command[1]);
+                    break;
+                case "Rename":
+                    article.Rename(command[1]);
+                    break;
+            }
+        }
+
+        Console.WriteLine(article);
+    }
+}
---
+++ Students/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public float Grade { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<Student> students = new List<Student>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] studentInfo = Console.ReadLine().Split(' ');
+            students.Add(new Student
+            {
+                FirstName = studentInfo[0],
+                LastName = studentInfo[1],
+                Grade = float.Parse(studentInfo[2])
+            });
+        }
+
+        foreach (var student in students.OrderByDescending(s => s.Grade))
+        {
+            Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+        }
+    }
+}
